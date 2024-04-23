37d7082d6732ffff1d18a73c870f035f2d074cd6
davidearbola <davidearbola@gmail.com>
2024-04-23 18:40:54 +0200

    soluzione top con decremento (bottone previous)

--- js/main.js
+++ js/main.js
@@ -1,23 +1,77 @@
-let activeSlide;
-let nextSlide;
-let slideList = document.getElementsByClassName(`slide`);
+// let activeSlide;
+// let nextSlide;
+// let slideList = document.getElementsByClassName(`slide`);
 
-document.getElementById(`next`).addEventListener(`click`, function(){
-    for (let i = 0; i < slideList.length; i++){
-        const slide = slideList[i];
-        if (slide.classList.contains(`active`)){
-            slide.classList.remove(`active`);
-            activeSlide = i;
-        }
-    }
+// document.getElementById(`next`).addEventListener(`click`, function(){
+//     for (let i = 0; i < slideList.length; i++){
+//         const slide = slideList[i];
+//         if (slide.classList.contains(`active`)){
+//             slide.classList.remove(`active`);
+//             activeSlide = i;
+//         }
+//     }
     
-    nextSlide = activeSlide + 1;
+//     nextSlide = activeSlide + 1;
     
-    if (nextSlide >= slideList.length){
-        nextSlide = 0;
-    }
+//     if (nextSlide >= slideList.length){
+//         nextSlide = 0;
+//     }
     
-    slideList[nextSlide].classList.add(`active`);
-})
+//     slideList[nextSlide].classList.add(`active`);
+// })
 
+
+//recupero tutte le slide e le metto in un array
+let slides = document.getElementsByClassName("slide");
+
+//conto le slide
+//ora so che posso impostare come active una slide con indice tra (0) e (nSlides-1)
+let nSlides = slides.length
+
+//so che al caricamento della pagina l'active in html sarà sulla prima immagine
+let slideSelected = 0;
+
+//Al click sul pulsante
+document.getElementById("next").addEventListener("click", function () {
+
+	//Rimuovo l'active attuale
+	slides[slideSelected].classList.remove("active");
+	console.log("Selezione -> rimuovi", slideSelected);
+	
+	//se non sono arrivato all'ultima immagine
+	if( slideSelected < nSlides - 1 ){
+		//la nuova selected è la prossima
+		slideSelected++;
+	} else {
+		//la nuova selected è la prima
+		slideSelected = 0;
+	}
+	
+	//aggiungo l'active alla successiva
+	slides[slideSelected].classList.add("active");
+	console.log("Selezione -> aggiungi", slideSelected);
+});
+
+
+//Esempio di event handler
+document.getElementById("previous").addEventListener("click", function () {
+    //Rimuovo l'active attuale
+	slides[slideSelected].classList.remove("active");
+	console.log("Selezione -> rimuovi", slideSelected);
+    //se non sono arrivato all'ultima immagine
+	if( slideSelected > 0){
+		//la nuova selected è la prossima
+		slideSelected--;
+	} else {
+		//la nuova selected è la prima
+		slideSelected = 4;
+	}
+
+    //aggiungo l'active alla successiva
+	slides[slideSelected].classList.add("active");
+	console.log("Selezione -> aggiungi", slideSelected);
+
+
+    
+});
 
