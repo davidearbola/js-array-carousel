714e039f29106af9293c2bac9b5064f80d5e0566
davidearbola <davidearbola@gmail.com>
2024-04-23 17:26:09 +0200

    creo ciclo per cambiare le classi alle immagini

--- js/main.js
+++ js/main.js
@@ -3,3 +3,20 @@ let nextSlide;
 let slideList = document.getElementsByClassName(`slide`);
 
 
+for (let i = 0; i < slideList.length; i++){
+    const slide = slideList[i];
+    if (slide.classList.contains(`active`)){
+        slide.classList.remove(`active`);
+        activeSlide = i;
+    }
+}
+
+nextSlide = activeSlide + 1;
+
+if (nextSlide >= slideList.length){
+    nextSlide = 0;
+}
+
+slideList[nextSlide].classList.add(`active`);
+
+
