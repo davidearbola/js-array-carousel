8b441d26355b0eee55c012addff9ef168aa279a6
davidearbola <davidearbola@gmail.com>
2024-04-23 17:27:36 +0200

    creo event listener per far cmabiare l'immagine visualizzata ad ogni click sul bottone

--- js/main.js
+++ js/main.js
@@ -2,21 +2,22 @@ let activeSlide;
 let nextSlide;
 let slideList = document.getElementsByClassName(`slide`);
 
-
-for (let i = 0; i < slideList.length; i++){
-    const slide = slideList[i];
-    if (slide.classList.contains(`active`)){
-        slide.classList.remove(`active`);
-        activeSlide = i;
+document.getElementById(`next`).addEventListener(`click`, function(){
+    for (let i = 0; i < slideList.length; i++){
+        const slide = slideList[i];
+        if (slide.classList.contains(`active`)){
+            slide.classList.remove(`active`);
+            activeSlide = i;
+        }
     }
-}
-
-nextSlide = activeSlide + 1;
-
-if (nextSlide >= slideList.length){
-    nextSlide = 0;
-}
-
-slideList[nextSlide].classList.add(`active`);
+    
+    nextSlide = activeSlide + 1;
+    
+    if (nextSlide >= slideList.length){
+        nextSlide = 0;
+    }
+    
+    slideList[nextSlide].classList.add(`active`);
+})
 
 
